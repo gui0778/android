1d06e4b145e5d7dfe9707e8458007c94f8c11f7a
gui0778 <you@example.com>
2013-08-30 23:10:08 +0800

    tcy sb

---
+++ DataBaseManager/src/com/tcy/database/DataBaseManagerActivity.java
@@ -0,0 +1,13 @@
+package com.tcy.database;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DataBaseManagerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
